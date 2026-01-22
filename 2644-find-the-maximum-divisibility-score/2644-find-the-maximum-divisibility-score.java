f1cfebe5f766838eb43e92f1980c1ef9032567ca
harshithpavansai <harshithpavansai188@gmail.com>
2026-01-22 12:02:01 +0530

    Time: 179 ms (50.24%), Space: 47.8 MB (19.47%) - LeetHub

---
+++ 2644-find-the-maximum-divisibility-score/2644-find-the-maximum-divisibility-score.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxDivScore(int[] nums, int[] divisors) {
+        int maxCount = -1;
+        int bestDivisor = Integer.MAX_VALUE;
+
+        for (int i = 0; i < divisors.length; i++) {
+            int cnt = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] % divisors[i] == 0) {
+                    cnt++;
+                }
+            }
+
+            if (cnt > maxCount || (cnt == maxCount && divisors[i] < bestDivisor)) {
+                maxCount = cnt;
+                bestDivisor = divisors[i];
+            }
+        }
+
+        return bestDivisor;
+    }
+}
